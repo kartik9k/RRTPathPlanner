4413d99879af4292a8d9f8f8dd7c248a942999bf
kartik9k <kartik9k@gmail.com>
2018-02-10 20:28:26 +0530

    Added files from Lab computer

---
+++ rrtStar.py
@@ -0,0 +1,109 @@
+import math
+import random
+import copy
+
+class Node():
+	def __init__(self, x, y, par = None, cost = 0):
+		self.x = x
+		self.y = y
+		self.cost = cost
+		self.parent = par
+
+class RRT():
+	def __init__(self, start, goal, obstacleList, expandDist, goalSampleRate):
+		self.start = Node(start[0], start[1])
+		self.goal = Node(goal[0], goal[1])
+		self.obstacleList = obstacleList
+		self.expandDist = expandDist
+		self.goalSampleRate = goalSampleRate
+
+	def planner(self):
+		self.nodeList = [self.start]
+		xPlot = []
+		yPlot = []
+		while True and self.goal not in self.nodeList:
+			if random.random() > self.goalSampleRate:
+				rnd = [self.goal.x, self.goal.y]
+			else:
+				rnd = [random.randint(0, 100), random.randint(0, 100)]
+
+			neareastIndex = self.getNearestIndex(rnd)
+			nearestNode = self.nodeList[neareastIndex]
+			
+			theta = math.atan2(rnd[1] - nearestNode.y, rnd[0] - nearestNode.x)
+
+			xNew = nearestNode.x + self.expandDist * math.cos(theta)
+			yNew = nearestNode.y + self.expandDist * math.sin(theta)
+
+			if self.doesNotCollide(xNew, yNew):
+				newNode = Node(xNew, yNew, neareastIndex, nearestNode.cost + 1)
+				
+				nearInd = self.nearIndex(newNode)
+				possibleConnections = self.checkConnection(newNode, nearIndex)
+
+				leftx = newNode.x - self.goal.x
+				lefty = newNode.y - self.goal.y
+
+				xPlot.append(newNode.x)
+				yPlot.append(newNode.y)
+
+				if (math.sqrt(leftx * leftx + lefty * lefty) <= self.expandDist):
+					self.goal.parent = neareastIndex
+					self.nodeList.append(self.goal)
+					print("Goal Reached :D")
+				
+				else:
+					self.nodeList.append(newNode)
+			
+			else:
+				continue
+
+		pathX = []
+		pathY = []
+
+		lastInd = len(self.nodeList) - 1
+		while self.nodeList[lastInd].parent != None:
+			node = self.nodeList[lastInd]
+			pathX.append(node.x)
+			pathY.append(node.y)
+			lastInd = node.parent
+
+		pathX.append(self.start.x)
+		pathY.append(self.start.y)
+		
+		return pathX, pathY, xPlot, yPlot
+
+	def getNearestIndex(self, rnd):
+		distances = []
+		for node in self.nodeList:
+			distances.append((node.x - rnd[0]) ** 2 + (node.y - rnd[1]) ** 2)
+		return distances.index(min(distances))
+
+	def doesNotCollide(self, x, y):
+		for (ox, oy, dx, dy) in self.obstacleList:
+			if (abs(x - ox) <= dx/2.0 and abs(y - oy) <= dy/2.0):
+				return False
+		return True
+
+	def nearIndex(self, newNode):
+		distances = []
+		for node in self.nodeList:
+			distances.append(math.sqrt((node.x - newNode.x) ** 2 + (node.y - newNode.y) ** 2))
+
+		r = 3.0 * math.sqrt((math.log(len(self.nodeList)))/ len(self.nodeList))
+		nearInd = []
+		for i in range(len(distances)):
+			if distances[i] <= r:
+				nearInd.append(i)
+
+		return nearInd
+
+	def checkConnection(self,newNode, nearIndex):
+		if len(nearIndex) == 0:
+			return newNode
+
+		distance = []
+		for ind in nearIndex:
+			dx = newNode.x - self.nodeList[ind].x
+			dy = newNode.y - self.nodeList[ind].y
+			dist = dx ** 2 + dy ** 2
